a826101ca7fccdfcc4d1afc453a22c9585170317
SURYA-200346 <surya.jsdev@gmail.com>
2025-12-23 21:02:00 +0530

    add delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,23 @@ function App() {
     setResult([...result, input]);
     setInput("");
   }
-
+  const handleDelete = (id) => {
+    const updatetodo = result.filter((item) => item.id !== id);
+    setResult(updatetodo);
+  }
   return (
     <>
       <div className="container">
         <input value={input} onChange={(e) => setInput(e.target.value)} type="text" name="text" id="" placeholder='Enter Task...' autoComplete='off' />
         {error && <p>{error}</p>}
         <button onClick={handleClick}>Add</button>
+
         <div>
           <ul>
-            {result.map((todo, index) => <li key={index}>{todo}</li>)}
-         
-          </ul>
-        </div>
+            {result.map((todo, index) => <li key={index}>{todo}< button className='btn' onClick={() => handleDelete()}>❌</button></li>) }
+        </ul>
       </div>
+    </div >
 
     </>
   )
